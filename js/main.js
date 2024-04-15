662d48a5b6769ad099b76509b64eaa55b6751e24
papoy <m.dahlan772@gmail.com>
2024-04-15 14:22:18 +0700

    Pembaruan Profile 15 April 2024

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 // Toggle active
-const navbarNav = document.querySelector(".navbar");
+const navbarNav = document.querySelector(".navbar-nav");
+const menu = document.querySelector(".navbar-extra");
 
 // Menu Di Klik
 document.querySelector("#menu-icon").onclick = () => {
   navbarNav.classList.toggle("active");
+  menu.classList.toggle("active"); // Menambah/menghapus kelas "active" pada ikon menu
 };
 
 // Klik di Luar side Bar untuk menutup Navbar
-const menu = document.querySelector("#menu-icon");
-
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+    menu.classList.remove("active"); // Menghapus kelas "active" pada ikon menu jika di luar side bar
   }
 });

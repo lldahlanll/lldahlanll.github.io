aa49d494f729e6a667fdab7517da27d6c5db8acd
lldahlanll <44283252+lldahlanll@users.noreply.github.com>
2024-04-18 22:11:06 +0700

    Update script.js
    perubahan di bagian scroll dan navbar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,70 @@
-// toggle class active
+// Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
+const menuIcon = document.querySelector("#menu-icon");
 
-// ketika bakso menu di klik
-document.querySelector("#profile-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+menuIcon.onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan navbarNav
-const bakso = document.querySelector("#profile-menu");
-
+// Hide navbarNav when clicked outside
 document.addEventListener("click", function (e) {
-    if (!bakso.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove("active");
+  if (!menuIcon.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Toggle Icon
+const menuIcons = document.querySelectorAll("#menu-icon");
+const navbars = document.querySelectorAll(".navbar-nav");
+
+menuIcons.forEach((menuIcon, index) => {
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbars[index].classList.toggle("active");
+  };
+});
+
+// Scroll Section Active Link
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar-nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector(".navbar-nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
     }
+  });
+
+  // Sticky Navbar
+  let navbar = document.querySelector(".navbar");
+  if (navbar) {
+    navbar.classList.toggle("sticky", window.scrollY > 100);
+  }
+
+  let menuIcon = document.querySelector("#menu-icon");
+  if (menuIcon) {
+    menuIcon.classList.remove("bx-x");
+    navbars.forEach((navbar) => {
+      navbar.classList.remove("active");
+    });
+  }
+};
+
+// Typing Text
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Mobile Developer", "IT Support"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
